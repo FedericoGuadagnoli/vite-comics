9d1ab544a9b7af01c15490af61b65b0332d4d6aa
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-20 14:51:32 +0100

    Add macro-areas , add generics rules css header

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,43 @@
 </script>
 
 <template>
+  <!-- Header -->
+  <header>
+    <div class="container">
+      <div class="navbar">
+
+      </div>
+    </div>
+  </header>
+  <main></main>
+  <footer></footer>
 
 </template>
 
 <style>
+/* RESET */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+/* GENERICS */
+.container {
+  width: 80%;
+  margin: 0 auto;
+}
+
+
+/* HEADER */
+header {
+  height: 100px;
+}
+
+.navbar {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
