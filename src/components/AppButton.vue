a3051776e275bb44ff0b35a910eb5e5e80a66f0b
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-23 14:53:13 +0100

    Create component AppButton.vue

---
+++ src/components/AppButton.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'AppButton'
+}
+</script>
+
+
+<template>
+    <div>
+        <button>load more</button>
+    </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/mixins' as *;
+
+div {
+    @include center('center');
+    padding: 2rem 0;
+
+    button {
+        text-transform: uppercase;
+        color: white;
+        padding: .8rem 3rem;
+        font-size: 1.25rem;
+        background-color: dodgerblue;
+        cursor: pointer;
+    }
+}
+</style>
