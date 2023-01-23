7c0dbc3ae009ecdb4a343a1a6179de2095499d0f
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-23 14:34:22 +0100

    Create component AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+    name: 'AppMain',
+    props: {
+        cards: Array
+    }
+};
+</script>
+
+
+<template>
+    <div class="row">
+        <div class="card" v-for="card in cards" :key="card.series">
+            <figure>
+                <img :src="card.thumb" :alt="card.series">
+            </figure>
+            <figcaption>{{ card.series }}</figcaption>
+        </div>
+    </div>
+
+</template>
+
+
+<style scoped lang="scss">
+.row {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    gap: 2rem;
+
+    .card {
+        flex-basis: calc(100% / 6 - 2rem);
+        height: 350px;
+
+        figure {
+            height: 80%;
+
+            img {
+                width: 100%;
+                height: 100%;
+                display: block;
+            }
+        }
+
+        figcaption {
+            margin-top: 1rem;
+        }
+    }
+}
+</style>
